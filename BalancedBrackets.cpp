a9c476284b7fd3fb53f37dfafbb46823a887a350
Himanshupate <41292696+Himanshupate@users.noreply.github.com>
2019-03-05 20:10:37 +0530

    Create BalancedBrackets.cpp

---
+++ BalancedBrackets.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+
+int main() {
+  int i,j,k,l;
+  stack <char> ss;
+  
+  string s;
+  cin>>s;
+  
+  ss.push(s[0]);
+  for(i=1;i<s.length();i++){
+     switch (s[i])
+     {
+        case '}': if(ss.top()=='{'){
+                        ss.pop();        
+                  } 
+                    break;
+        case '>': if(ss.top()=='<'){
+                        ss.pop();        
+                  }
+                    break;
+        case ')': if(ss.top()=='('){
+                        ss.pop();        
+                  }
+                    break;
+        default : ss.push(s[i]); 
+     }
+     
+  }
+  if(ss.empty()){
+      cout<<"String is Balanced.";
+  }
+  else
+  cout<<"String is Not Balanced.";
+}
